216674221428dff530c0365dac028eb8728ea8ff
Michael Steil <mist64@mac.com>
2019-09-01 22:04:52 +0200

    more sprites!

--- sprite-demo.asm
+++ sprite-demo.asm
@@ -12,22 +12,35 @@
 	lda #1
 	+vstore vreg_spr
 
+MAX_SPRITES = 16
+MAX_X = 640
+MAX_Y = 480
+
 spr_data = $10000
 
 ; configure sprite #0
 
-	lda #1 << 4
-	+vstore vreg_sprd + 1
-
+	ldx #0
+setup:
+	txa
+	asl
+	asl
+	asl
+	asl
+	+sprstore 1 ; palette offset
 	lda #3 << 2 | 1 << 1 ; z-depth=3, mode=1
-	+vstore vreg_sprd + 3
-
+	+sprstore 3
 	lda #<spr_data
-	+vstore vreg_sprd + 4
+	+sprstore 4
 	lda #8 | 1 << 4 | 2 << 6 | >spr_data
-	+vstore vreg_sprd + 5
+	+sprstore 5
+	inx
+	cpx #MAX_SPRITES
+	beq setup_done
+	jmp setup
+setup_done:
 
-; set up sprite #0 shape
+; set up sprite shape
 	+vset spr_data
 
 	ldy #1 ; white
@@ -54,21 +67,56 @@ l4:	lda sprite + $300,x
 	inx
 	bne l4
 
+loop:
 	ldx #0
-
-loop:	txa
-	+vstore vreg_sprd
-;	+vload vreg_sprd + 1
-;	and #%11111100
-;	ora #0
-;	+vstore vreg_sprd + 1
-	txa
-	+vstore vreg_sprd + 2
-	+vload vreg_sprd + 3
+lo2:
+	lda sprx_lo,x
+	+sprstore 0
+	+sprload 1
+	and #%11111100
+	ora sprx_hi,x
+	+sprstore 1
+	lda spry_lo,x
+	+sprstore 2
+	+sprload 3
 	and #%11111110
-	+vstore vreg_sprd + 3
+	ora spry_hi,x
+	+sprstore 3
 
-	lda #20
+	inc sprx_lo,x
+	bne ll1
+	inc sprx_hi,x
+ll1:	lda sprx_lo,x
+	sec
+	sbc #<MAX_X
+	lda sprx_hi,x
+	sbc #>MAX_X
+	bcc ll2
+	lda #0
+	sta sprx_lo,x
+	sta sprx_hi,x
+ll2:
+
+	lda spry_lo,x
+	sec
+	sbc #1
+	sta spry_lo,x
+	lda spry_hi,x
+	sbc #0
+	sta spry_hi,x
+	bcs ll4
+	lda #<MAX_Y
+	sta spry_lo,x
+	lda #>MAX_Y
+	sta spry_hi,x
+ll4:
+	inx
+	cpx #MAX_SPRITES
+	beq nlo2
+	jmp lo2
+nlo2:
+
+	lda #10
 	ldy #0
 delay:	dey
 	bne delay
@@ -76,7 +124,6 @@ delay:	dey
 	sbc #1
 	bne delay
 
-	inx
 	jmp loop
 
 convert_color:
@@ -90,13 +137,13 @@ cc1:	tya
 sprite:
 !text "....*******....."
 !text "..***********..."
-!text ".*************.."
-!text ".*****..******.."
-!text "*****.**..*****."
-!text "*****.*********."
-!text "*****.**..*****."
-!text ".*****..******.."
-!text ".*************.."
+!text ".***.*****.***.."
+!text ".****.***.****.."
+!text "******.*.******."
+!text "******.*.******."
+!text "******.*.******."
+!text ".****.***.****.."
+!text ".***.*****.***.."
 !text ".*************.."
 !text ".*.*********.*.."
 !text "..*.*******.*..."
@@ -120,3 +167,12 @@ sprite:
 !text "................"
 !text "................"
 !text "................"
+
+sprx_lo:
+	!byte 216, 47, 111, 171, 118, 34, 129, 97, 164, 168, 41, 29, 228, 207, 140, 17
+sprx_hi:
+	!fill MAX_SPRITES, 0
+spry_lo:
+	!byte 221, 223, 182, 216, 50, 181, 147, 234, 164, 219, 251, 168, 14, 155, 141, 83
+spry_hi:
+	!fill MAX_SPRITES, 0
