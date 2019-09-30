53954b03705b9dabe66ddd8cebc95e988f45489f
Frank Buss <55055211+Frank-Buss@users.noreply.github.com>
2019-09-30 03:09:52 +0200

    cleanups for new CC65 version with cx16 target (#36)
    * cleanup for new CC65 version and better VGM player
    * ported to new CC65 version and smoother animation
    * changed comment about cc65 version

--- cc65-sprite/demo.c
+++ cc65-sprite/demo.c
@@ -25,14 +25,16 @@
 
 // Demonstrates how to initialize and move sprites with cc65, synchronized
 // to the VSync interrupt.
-// Tested with cc65 V2.18.
+// Tested with the latest unreleased cc65, V2.18 - Git 18afc7c7
 //
 // Generate assembler listing to demo.txt:
-// cl65 -t c64 -O -l demo.txt demo.c
+// cl65 -t cx16 -O -l demo.txt demo.c
 //
 // See Makefile how to compile it and how to convert a PNG image to a C array.
 
 #include <stdint.h>
+#include <stdio.h>
+#include <conio.h>
 #include <6502.h>
 
 #include "balloon.inc"
@@ -42,43 +44,31 @@
 /*
 Python script to generate the table:
 import math
-cycle=56
-ampl=50
-[int(math.sin(i/cycle*2*math.pi)*ampl+0.5) for i in range(cycle)]
+cycle=100
+ampl=50.0
+[int(math.sin(float(i)/cycle*2.0*math.pi)*ampl+0.5) for i in range(cycle)]
 */
 static int sin[] = {
-	0, 6, 11, 17, 22, 27, 31, 35, 39, 42, 45, 47, 49, 50, 50,
-	50, 49, 47, 45, 42, 39, 35, 31, 27, 22, 17, 11, 6, 0, -5,
-	-10, -16, -21, -26, -30, -34, -38, -41, -44, -46, -48, -49,
-	-49, -49, -48, -46, -44, -41, -38, -34, -30, -26, -21, -16,
-	-10, -5
+    0, 3, 6, 9, 12, 15, 18, 21, 24, 27, 29, 32, 34, 36, 39, 40, 42,
+    44, 45, 46, 48, 48, 49, 50, 50, 50, 50, 50, 49, 48, 48, 46, 45,
+    44, 42, 40, 39, 36, 34, 32, 29, 27, 24, 21, 18, 15, 12, 9, 6, 3,
+    0, -2, -5, -8, -11, -14, -17, -20, -23, -26, -28, -31, -33, -35,
+    -38, -39, -41, -43, -44, -45, -47, -47, -48, -49, -49, -49, -49,
+    -49, -48, -47, -47, -45, -44, -43, -41, -39, -38, -35, -33, -31,
+    -28, -26, -23, -20, -17, -14, -11, -8, -5, -2
 };
-
-struct VERA_t {
-    uint8_t lo;
-    uint8_t mid;
-    uint8_t hi;
-    uint8_t data1;
-    uint8_t data2;
-    uint8_t ctrl;
-    uint8_t ien;
-    uint8_t isr;
-};
-
-#define VERA (*(volatile struct VERA_t*) 0x9f20)
 
 static uint8_t ofs = 0;
 
 static void vpoke(uint8_t bank, uint16_t address, uint8_t data)
 {
     // address selection 0
-    VERA.ctrl = 0;
+    VERA.control = 0;
     // set address
-    VERA.hi = bank;
-    VERA.mid = address >> 8;
-    VERA.lo = address & 0xff;
-    // store data with data port 1
-    VERA.data1 = data;
+    VERA.address_hi = bank;
+    VERA.address = address;
+    // store data with data port 0
+    VERA.data0 = data;
 }
 
 static unsigned char irq()
@@ -86,29 +76,30 @@ static unsigned char irq()
     uint8_t j = ofs;
     uint8_t i;
 
-    // clear interrupt flags
-    VERA.isr = 1;
-
     // update sprite y coordinate
     for (i = 0; i < SPRITE_COUNT; i++) {
         uint16_t adr = i * 8;
         vpoke(0xf, 0x5004 + adr, 80 + sin[j]);
         j += 4;
-        if (j > 55) j -= 56;
+        if (j > 99) j -= 100;
     }
     
     // update start offset
     ofs++;
-    if (ofs == 56) ofs = 0;
+    if (ofs == 100) ofs = 0;
     
     // return from interrupt
     return IRQ_HANDLED;
 }
 
-static void main2()
+int main()
 {
     uint16_t i = 0;
 
+    // switch back to uppercase character set
+    __asm__("lda #$8e");
+    __asm__("jsr BSOUT");
+    
     // initialize sprite information
     for (i = 0; i < SPRITE_COUNT; i++) {
         uint16_t adr = i * 8;
@@ -150,20 +141,12 @@ static void main2()
     set_irq(irq, (void*) 0xa800, 0x0800);
     
     // the rest runs in the interrupt
-    while (1);
-}
-
-int main()
-{
-    // switch back to uppercase character set
-    __asm__("lda #$8e");
-    __asm__("jsr BSOUT");
+    printf("press any key to stop\n");
+    while (!kbhit());
+    cgetc();
     
-    // bad hack: redefine CC65 stack to $0xa800-0xafff, should be a proper x16 custom target
-    *((uint16_t*) 0x02) = 0xb000;
-    
-    // call new main function, because local variables are not valid anymore after the stack change
-    main2();
+    // disable sprites
+    vpoke(0xf, 0x4000, 0);
     
 	return 0;
 }
